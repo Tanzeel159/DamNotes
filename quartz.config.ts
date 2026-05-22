77959399c063413a6a825a15efd2eccaca687741
Tanzeel159 <20295349+Tanzeel159@users.noreply.github.com>
2026-05-22 12:49:22 -0500

    renamed the website

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "DamNotes",
+    pageTitle: "Damlog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
