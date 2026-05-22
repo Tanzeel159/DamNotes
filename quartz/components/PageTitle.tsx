c5b13edadb6a741de9047afde81367e91aeab231
Tanzeel159 <20295349+Tanzeel159@users.noreply.github.com>
2026-05-22 12:03:47 -0500

    updated image

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <img src="/static/beaver-logo.svg" alt="Beaver logo" class="page-title-logo" />
+      <img src="/static/beaver-logo.png" alt="Beaver logo" class="page-title-logo" />
       <a href={baseDir}>{title}</a>
     </h2>
   )
